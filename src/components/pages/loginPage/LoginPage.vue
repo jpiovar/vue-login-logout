c01c57f2a8fbd9bffe2009b73fe03b8cbc1a0cea
jpiovar <jpiovar@sk.ibm.com>
2019-01-26 01:34:33 +0100

    debugger out

--- src/components/pages/loginPage/LoginPage.vue
+++ src/components/pages/loginPage/LoginPage.vue
@@ -34,14 +34,12 @@ export default class LoginPage extends Vue {
 
   @UserStore.Action loginUser: any;
 
-  loginHandle(userAuth: {name: string, password: string}) {
-    debugger;
+  loginHandle(userAuth: {name: string, password: string}) {    
     console.log('login clicked');
     const up = userProfiles.filter(item => item.name === userAuth.name && item.password === userAuth.password);
     if (up && up.length > 0) {
       this.loginUser({'id': up[0].id, 'name': up[0].name, 'email': up[0].email});
     } else {
-      debugger;
       (this.$refs.loginForm as HTMLFormElement).showAlertBannerAndReset();
     }
     // axios
