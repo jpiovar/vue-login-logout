b2d897e49743da990e5cb6c4c3e6e616af661c0b
jpiovar <jpiovar@sk.ibm.com>
2019-01-28 20:42:44 +0100

    login form extended condition

--- src/components/pages/loginPage/components/LoginForm.vue
+++ src/components/pages/loginPage/components/LoginForm.vue
@@ -6,18 +6,29 @@
       <form>
         <div class="form-group">
           <label for="userName">User name</label>
-          <input type="text" v-model="userNameVal" @keydown.enter="loginForm" ref="userName" class="form-control" id="userName" aria-describedby="userNameHelp" placeholder="Enter user name">
-          <!-- <small id="userNameHelp" class="form-text text-muted">Please use valid user name.</small> -->
+          <input type="text"
+            v-model="userNameVal"
+            @keydown.enter="loginForm"
+            ref="userName" class="form-control" id="userName"
+            aria-describedby="userNameHelp" placeholder="Enter user name">
         </div>
         <div class="form-group">
           <label for="exampleInputPassword1">Password</label>
-          <input type="password" v-model="passwordVal" @keydown.enter="loginForm" class="form-control" id="exampleInputPassword1" placeholder="Password">
+          <input type="password"
+            v-model="passwordVal"
+            @keydown.enter="loginForm"
+            class="form-control" id="exampleInputPassword1" placeholder="Password">
         </div>
-        <button type="submit" class="btn btn-primary" @click.prevent="loginForm" @keydown.tab.prevent="setFocus">Login</button>
+        <button type="submit" class="btn btn-primary"
+          @click.prevent="loginForm"
+          @keydown.tab.prevent="setFocus">Login
+        </button>
       </form>
       <div class="alert alert-warning alert-dismissible fade show" role="alert" v-if='showAlert'>
         Please use valid credentials.
-        <button type="button" class="close" ref="alertBtn" aria-label="Close" @click.prevent="hideAlertBanner" @keydown.tab.prevent="setFocusInput">
+        <button type="button" class="close" ref="alertBtn" aria-label="Close"
+          @click.prevent="hideAlertBanner"
+          @keydown.tab.prevent="setFocusInput">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
@@ -34,18 +45,21 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
   props: {
     loginRef: {
       required: true,
-      type: Function
-    }
-  }
+      type: Function,
+    },
+  },
 })
 export default class LoginForm extends Vue {
   loginRef!: any;
+
   userNameVal: string = '';
+
   passwordVal: string = '';
+
   showAlert: boolean = false;
 
   loginForm() {
-    this.loginRef({'name': this.userNameVal, 'password': this.passwordVal});
+    this.loginRef({ name: this.userNameVal, password: this.passwordVal });
   }
 
   showAlertBannerAndReset() {
@@ -53,16 +67,20 @@ export default class LoginForm extends Vue {
     this.userNameVal = '';
     this.passwordVal = '';
   }
+
   hideAlertBanner() {
     this.showAlert = false;
     (this.$refs.userName as HTMLFormElement).focus();
   }
+
   setFocus() {
-    if(!this.showAlert) {
+    if (!this.showAlert) {
       (this.$refs.userName as HTMLFormElement).focus();
+    } else if (this.showAlert && this.$refs.alertBtn) {
+      (this.$refs.alertBtn as HTMLFormElement).focus();
     }
-    (this.$refs.alertBtn as HTMLFormElement).focus();
   }
+
   setFocusInput() {
     (this.$refs.userName as HTMLFormElement).focus();
   }
