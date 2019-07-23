55be381932e12c0dc75011008da9c020b0f7326a
Juraj Piovar <jpiovar@sk.ibm.com>
2019-07-23 19:21:11 +0200

    codeclimate wrapper refactor

--- src/components/pages/quoraPage/components/answerCmp/__tests__/AnswerCmp.spec.ts
+++ src/components/pages/quoraPage/components/answerCmp/__tests__/AnswerCmp.spec.ts
@@ -13,25 +13,30 @@ const getWrapperShallow = (propsData = props): Wrapper<AnswerCmp> => shallowMoun
     },
 });
 
+let wrapper: Wrapper<AnswerCmp>;
+
 describe('AnswerCmp.vue', () => {
+    beforeEach(() => {
+        wrapper = getWrapperShallow();
+    });
     it('should match the snapshot', () => {
-        const wrapper: Wrapper<AnswerCmp> = getWrapperShallow();
+        // const wrapper: Wrapper<AnswerCmp> = getWrapperShallow();
         expect(wrapper.vm.$el).toMatchSnapshot()
     });
     it('renders card-header', () => {
-        const wrapper: Wrapper<AnswerCmp> = getWrapperShallow();
+        // const wrapper: Wrapper<AnswerCmp> = getWrapperShallow();
         const cardHeader = wrapper.find('.card-header');
         expect(cardHeader.exists()).toBe(true);
         expect(cardHeader.isVisible()).toBe(true);
     });
     it('renders card-body', () => {
-        const wrapper: Wrapper<AnswerCmp> = getWrapperShallow();
+        // const wrapper: Wrapper<AnswerCmp> = getWrapperShallow();
         const cardBody = wrapper.find('.card-body');
         expect(cardBody.exists()).toBe(true);
         expect(cardBody.isVisible()).toBe(true);
     });
     it('renders component id with correct string', () => {
-        const wrapper: Wrapper<AnswerCmp> = getWrapperShallow();
+        // const wrapper: Wrapper<AnswerCmp> = getWrapperShallow();
         expect(wrapper.attributes().id).toBe('a12');
     });
 });
