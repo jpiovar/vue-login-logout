9ada2dc8a7c37e85b00ddd71b20ae47eb9cb9b07
Juraj Piovar <jpiovar@sk.ibm.com>
2019-07-23 19:42:09 +0200

    codeclimate loginPage unittests optimization

--- src/components/pages/loginPage/__tests__/LoginPage.spec.ts
+++ src/components/pages/loginPage/__tests__/LoginPage.spec.ts
@@ -32,25 +32,23 @@ const getWrapperMount = (propsData = props): Wrapper<LoginPage> => mount(LoginPa
     stubs: ['LoginForm']
 });
 
-let wrapper: Wrapper<LoginPage>;
+const wrapper: Wrapper<LoginPage> = getWrapperShallow();
+const spanFooter = wrapper.find('span#footer');
 
 describe('LoginPage.vue', () => {
-    beforeEach(() => {
-        wrapper = getWrapperShallow();
-    });
     it('should match the snapshot', () => {
         // const wrapper: Wrapper<LoginPage> = getWrapperShallow();
         expect(wrapper.vm.$el).toMatchSnapshot()
     });
     it('renders footer exists and visible', () => {
         // const wrapper: Wrapper<LoginPage> = getWrapperShallow();
-        const spanFooter = wrapper.find('span#footer');
+        // const spanFooter = wrapper.find('span#footer');
         expect(spanFooter.exists()).toBe(true);
         expect(spanFooter.isVisible()).toBe(true);
     });
     it('renders footer with correct text', () => {
         // const wrapper: Wrapper<LoginPage> = getWrapperShallow();
-        const spanFooter = wrapper.find('span#footer');
+        // const spanFooter = wrapper.find('span#footer');
         const year = '2019';
         expect(spanFooter.text()).toBe(`${props.author} @ ${year}`);
     });
@@ -58,7 +56,7 @@ describe('LoginPage.vue', () => {
         // const wrapper: Wrapper<LoginPage> = getWrapperShallow();
         const mockData = { 'idComponent': 'loginPageIdComponent', 'year': '07/2019' };
         wrapper.setData(mockData); // sets component's data/instance attributes id, year with mock values
-        const spanFooter = wrapper.find('span#footer');
+        // const spanFooter = wrapper.find('span#footer');
         expect(wrapper.attributes().id).toBe('loginPageIdComponent'); // checks if component's DOM id is equal to value of set component's data/instance attribute id
         expect(spanFooter.text()).toBe(`${props.author} @ 07/2019`);
     });
